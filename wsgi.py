3f8d7dce2695321c6016d2ab99690b9f94529610
pgb3010 <47167589+pgb3010@users.noreply.github.com>
2019-02-06 08:56:31 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,11 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "Hello World!"
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
 
 if __name__ == "__main__":
     application.run()
